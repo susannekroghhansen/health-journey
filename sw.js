415fe209fa5aac243be138c61193684b942a028f
Susanne Krogh-Hansen <168450920+susannekroghhansen@users.noreply.github.com>
2026-06-14 07:54:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "healthjourney-v3";
+const CACHE = "healthjourney-v4";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png"];
 
